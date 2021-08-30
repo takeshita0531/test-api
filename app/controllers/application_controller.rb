ca1225fe24afcf7f64a33761f432efe0c5df7cdd
EC2 Default User <ec2-user@ip-172-31-8-147.us-east-2.compute.internal>
2021-08-30 06:03:39 +0000

    deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
         include DeviseTokenAuth::Concerns::SetUserByToken
-        before_action :authenticate_user!
+        # before_action :authenticate_user!
 #   protect_from_forgery with: :exception
         protect_from_forgery with: :null_session
         skip_before_action :verify_authenticity_token, if: :devise_controller?
