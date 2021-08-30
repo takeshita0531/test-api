f00d18ed256f81acc898aeb2557d8893f8996348
EC2 Default User <ec2-user@ip-172-31-8-147.us-east-2.compute.internal>
2021-08-30 06:07:12 +0000

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     scope :v1 do
-      mount_devise_token_auth_for "User", at: "auth"
-      # mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-      #   registrations: 'api/v1/auth/registrations'
-      # }
+      # mount_devise_token_auth_for "User", at: "auth"
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
     end
   end
 end
