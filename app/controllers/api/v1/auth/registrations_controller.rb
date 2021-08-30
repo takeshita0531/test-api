0094b029ecbb4eac198c88f5fbe405a9e59f5a0c
EC2 Default User <ec2-user@ip-172-31-8-147.us-east-2.compute.internal>
2021-08-30 06:58:04 +0000

    deploy

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -5,10 +5,10 @@ module Api
         skip_before_action :verify_authenticity_token
         private
         def sign_up_params
-          params.permit(:name, :nickname, :email, :image, :password, :password_confirmation)
+          params.require(:registration).permit(:name, :nickname, :email, :image, :password, :password_confirmation)
         end
         def account_update_params
-          params.permit(:name, :nickname, :email, :image)
+          params.require(:registration).permit(:name, :nickname, :email, :image)
         end
       end
     end
