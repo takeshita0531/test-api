7a7f509bd397d72d5db4f375bc37642dff664ea0
EC2 Default User <ec2-user@ip-172-31-8-147.us-east-2.compute.internal>
2021-09-03 06:27:07 +0000

    deploy

--- app/controllers/api/v1/user_news_controller.rb
+++ app/controllers/api/v1/user_news_controller.rb
@@ -1,24 +1,19 @@
 module Api
     module V1
         class UserNewsController < ApplicationController
-            
             def create
-                name = params[:name]
-                pwd = params[:pwd]
-                user = UserNew.new
-                user.naem = name
-                user.pwd = pwd
+                user = UserNew.new(user_new_params)
                 if user.save 
                     render json: { status: 'SUCCESS', data: user }
                 else
                     render json: { status: 'SUCCESS', data: user.errors }
                 end
             end
-            # private
+            private
 
-            # def user_new_params
-            #     params[:user_new].permit(:name, :pwd)
-            # end
+            def user_new_params
+                params.require(:user_new).permit(:name, :pwd)
+            end
         end
     end 
 end 
