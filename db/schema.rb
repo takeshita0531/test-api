7d52ef8b875f604e02353acc0e44adf3bce1524d
EC2 Default User <ec2-user@ip-172-31-8-147.us-east-2.compute.internal>
2021-09-03 04:52:55 +0000

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20210903043513) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "pwd"
+    t.string   "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_users_on_token", unique: true
+  end
 
 end
