9dff6b2b5657027881c01d645758cb783b04acfa
EC2 Default User <ec2-user@ip-172-31-8-147.us-east-2.compute.internal>
2021-09-03 05:15:29 +0000

    deploy

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+# module Api
+#   module V1
+#     module Auth
+#       class RegistrationsController < DeviseTokenAuth::RegistrationsController
+#         skip_before_action :verify_authenticity_token
+#         private
+#         def sign_up_params
+#           params.require(:registration).permit(:name, :nickname, :email, :image, :password, :password_confirmation)
+#         end
+#         def account_update_params
+#           params.require(:registration).permit(:name, :nickname, :email, :image)
+#         end
+#       end
+#     end
+#   end
+# end
